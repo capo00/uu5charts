88a61f5f1428e88b8ab96abe792dfc2559dd88a6
Ondřej Čapek <7-7948-1@plus4u.net>
2023-04-18 21:27:01 +0200

    remove deps, fix examples

--- uu5charts/src/exports.js
+++ uu5charts/src/exports.js
@@ -1,6 +1,3 @@
 // devkit-pure-exports
-export * as ReactChartJs2 from "./react-chartjs-2";
-export * as Recharts from "./recharts";
-
 export * from "./charts/xy-chart";
 export * from "./charts/pie-chart";
--- uu5charts/src/react-chartjs-2.js
+++
@@ -1,4 +0,0 @@
-export * from "./react-chartjs-2/line-chart.js";
-export * from "./react-chartjs-2/area-chart.js";
-export * from "./react-chartjs-2/bar-chart.js";
-export * from "./react-chartjs-2/pie-chart.js";
